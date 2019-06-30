889e6952f04222d20d839ed055b12cfe1d9aaa54
Aleksandr <kuang87@mail.ru>
2019-06-30 21:45:12 +0300

    new rest api products

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,39 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', function (){
+    return response()->json(['ack' => time()]);
+});
+
+Route::prefix('products')->group(function (){
+    Route::get('', function (){
+        $products = \App\Bakery::paginate(2);
+
+        return new \App\Http\Resources\BakeryCollection($products);
+    })->name('products.index');
+
+    Route::get('create', function (){
+        return response()->json(['route' => 'products.create']);
+    })->name('products.create');
+
+    Route::post('', function (){
+        return response()->json(['route' => 'products.store']);
+    })->name('products.store');
+
+    Route::get('{product}', function (\App\Bakery $product){
+        return new \App\Http\Resources\Bakery($product);
+    })->name('products.show');
+
+    Route::get('{product}/edit', function (){
+        return response()->json(['route' => 'products.edit']);
+    })->name('products.edit');
+
+    Route::match(['patch', 'put'], '{product}', function (){
+        return response()->json(['route' => 'products.update']);
+    })->name('products.update');
+
+    Route::delete('{product}', function (){
+        return response()->json(['route' => 'products.delete']);
+    })->name('products.delete');
+});
